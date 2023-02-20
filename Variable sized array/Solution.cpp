91ff760728910332c0314bbc2d4782399ac54019
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2023-02-20 21:09:46 +0530

    Create Solution.cpp

---
+++ Variable sized array/Solution.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    int temp;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+   for(int i = 0; i<n/2; i++){
+        temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
