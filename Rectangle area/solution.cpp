aaaebded03a38483e25df9c2af8dd8fc4a88c96d
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2023-02-21 05:15:38 +0530

    Create solution.cpp

---
+++ Rectangle area/solution.cpp
@@ -0,0 +1,24 @@
+//https://www.hackerrank.com/challenges/rectangle-area/problem?isFullScreen=true
+#include <iostream>
+
+using namespace std;
+/*
+ * Create classes Rectangle and RectangleArea
+ */
+class Rectangle{
+    public:
+    int width,height;
+    void display(){
+        cout<<width<<" "<<height<<endl;
+    }
+};
+class RectangleArea : public Rectangle{
+    public:
+    void read_input(){
+       cin>>width;
+       cin>>height;
+    }
+    void display(){
+        cout<<width*height;
+    }
+};
